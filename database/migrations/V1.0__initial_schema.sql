6629f7f4c1cce2c64db8757ba1e5f57028dca5ee
tom-ramirez <126895378+tom-ramirez@users.noreply.github.com>
2023-05-30 12:53:21 -0500

    We have the docker and server from the weekly-planner application, also testing that commits are working just fine now

---
+++ database/migrations/V1.0__initial_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS public.user
+(
+    id serial constraint user_pk primary key,
+    first_name text not null,
+    second_name text not null,
+    password text not null,
+    active boolean default true not null
+);
+
+CREATE TABLE IF NOT EXISTS public.planner
+(
+    id serial constraint planner_pk primary key,
+    start_date timestamp,
+    end_date timestamp,
+    user_id int constraint planner_user_user_id_fk references public.user(id)   
+);
+
+CREATE TABLE IF NOT EXISTS public.task (
+    id serial constraint task_pk primary key,
+	title text not null,
+    description text not null,
+	due timestamp,
+    accomplished boolean default false not null,
+	is_habit boolean default false not null,
+	planner_id int constraint task_planner_planner_id_fk references public.planner(id)   
+);
